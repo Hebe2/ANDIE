312a9e548009ea93e627e7fdb64561e6f2b53fe0
timnanevo <tnevo@macalester.edu>
2026-03-12 16:38:58 +1300

    updates to imagethreshold

--- src/main/java/cosc202/andie/ImageThresholding.java
+++ src/main/java/cosc202/andie/ImageThresholding.java
@@ -5,20 +5,22 @@
 package cosc202.andie;
 import java.awt.Color;
 import java.awt.image.BufferedImage;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 /**
  *
  * @author timnanevo
  */
 public class ImageThresholding implements ImageOperation{
-    private int value;
-    
-    public ImageThresholding(int value){
-        this.value = value;
+    private int threshold;
+ 
+    public ImageThresholding(int threshold){
+       this.threshold = threshold;
     }
+  
     
     @Override
     public BufferedImage apply(BufferedImage input) {
-
         for (int y = 0; y < input.getHeight(); ++y) {
             for (int x = 0; x < input.getWidth(); ++x) {
                 int rgb = input.getRGB(x, y);
@@ -27,7 +29,7 @@ public class ImageThresholding implements ImageOperation{
                 int g = color.getGreen();
                 int b = color.getBlue();
                 int intensity = (r+g+b)/3;
-                if (intensity >= value){
+                if (intensity >= threshold){
                     input.setRGB(x,y ,Color.WHITE.getRGB());
                 }
                 else{
