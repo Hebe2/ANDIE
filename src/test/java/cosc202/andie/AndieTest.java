907e6f38eb82d25853a91493c5d7a9e2d0bc28dd
timnanevo <tnevo@macalester.edu>
2026-03-16 16:27:40 +1300

    added andieTest file

---
+++ src/test/java/cosc202/andie/AndieTest.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package cosc202.andie;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author timnanevo
+ */
+public class AndieTest {
+    
+    public AndieTest() {
+    }
+    
+    @BeforeEach
+    public void setUp() {
+    }
+    
+    @AfterEach
+    public void tearDown() {
+    }
+    
+}
