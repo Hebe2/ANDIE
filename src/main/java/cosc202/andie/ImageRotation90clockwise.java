9f59521ee455e17ddf3cedf21510789e792ad6a3
hebebebebe <hebebebebe@hebes-air.byod.uod.otago.ac.nz>
2026-03-16 14:41:40 +1300

    90 clockwise done!!!!

--- src/main/java/cosc202/andie/ImageRotation90clockwise.java
+++ src/main/java/cosc202/andie/ImageRotation90clockwise.java
@@ -8,39 +8,35 @@ package cosc202.andie;
  *
  * @author hebebebebe
  */
-//import java.awt.Graphics2D;
+import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
-//import java.awt.geom.AffineTransform;
+import java.awt.geom.AffineTransform;
 
 public class ImageRotation90clockwise implements ImageOperation{
 
-//    public BufferedImage apply(BufferedImage input) {
-//        double rotationAngle = Math.toRadians(90);
-//
-//        int width = input.getWidth();
-//        int height = input.getHeight();
-//        
-//        BufferedImage output = new BufferedImage(newWidth, newHeight, input.getType());
-//        AffineTransform transform = new AffineTransform();
-//        transform.rotate(rotationAngle, newWidth / 2, newHeight / 2);
-//
-//        transform.translate((newWidth - width) / 2, (newHeight - height) / 2);
-//
-//        Graphics2D g2d = output.createGraphics();
-//        g2d.setTransform(transform);
-//        g2d.drawImage(input, 0, 0, null);
-//        g2d.dispose();
-//        return output;
-//    }
-     private final ImageRotation90clockwise rotator = new ImageRotation90clockwise();
-    
-    public ImageRotation90clockwise() {
-       
-    }
-    
-    @Override
     public BufferedImage apply(BufferedImage input) {
-        return rotator.apply(input);  
-    }
+        
+        
+        int width = input.getWidth();
+        int height = input.getHeight();
 
+        int newWidth = height;
+        int newHeight = width;
+        
+        BufferedImage output = new BufferedImage(newWidth, newHeight, input.getType());
+        
+        Graphics2D g2d = output.createGraphics();
+        
+        AffineTransform transform = new AffineTransform();
+         double angle = Math.toRadians(90);
+        transform.rotate(angle, newWidth / 2, newHeight / 2);
+
+        transform.translate((newWidth - width) / 2, (newHeight - height) / 2);
+  
+        g2d.setTransform(transform);
+        g2d.drawImage(input, 0, 0, null);
+        g2d.dispose();
+        return output;
+    }
+   
 }
