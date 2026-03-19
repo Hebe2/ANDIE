0239c61ed2d5f0682b48e56f426111bbf1ff0118
hebebebebe <hebebebebe@hebes-air.byod.uod.otago.ac.nz>
2026-03-19 14:26:58 +1300

    testing done

--- src/test/java/cosc202/andie/AndieTest.java
+++ src/test/java/cosc202/andie/AndieTest.java
@@ -4,6 +4,8 @@
  */
 package cosc202.andie;
 
+import java.awt.Dimension;
+import java.awt.image.BufferedImage;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -12,18 +14,35 @@ import static org.junit.jupiter.api.Assertions.*;
 /**
  *
  * @author timnanevo
+ * @author hebebebebe
  */
 public class AndieTest {
-    
+    private BufferedImage testImage;
+
     public AndieTest() {
     }
-    
+
     @BeforeEach
     public void setUp() {
+        testImage = new BufferedImage(200, 100, BufferedImage.TYPE_INT_RGB);
+
     }
-    
+
+    @Test
+    public void testImageSizeAfterResize() {
+        ImageResize half = new ImageResize(50);
+        BufferedImage resultH = half.apply(testImage);
+        assertEquals(100,resultH.getWidth());
+        assertEquals(50, resultH.getHeight());
+        
+        ImageResize doub = new ImageResize(200);
+        BufferedImage resultD = doub.apply(testImage);
+        assertEquals(400,resultD.getWidth());
+        assertEquals(200, resultD.getHeight());
+    }
+
     @AfterEach
     public void tearDown() {
     }
-    
+
 }
