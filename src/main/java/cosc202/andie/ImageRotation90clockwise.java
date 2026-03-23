78cd9c2510552d1c7971cfe465fb49a06f6b1134
hebebebebe <hebebebebe@hebes-air.byod.uod.otago.ac.nz>
2026-03-23 16:56:18 +1300

    java doc done img rotation
     Please enter the commit message for your changes. Lines starting

--- src/main/java/cosc202/andie/ImageRotation90clockwise.java
+++ src/main/java/cosc202/andie/ImageRotation90clockwise.java
@@ -5,52 +5,60 @@
 package cosc202.andie;
 
 /**
+ * <p>
+ * Rotates an image 90 degrees clockwise.
+ * </p>
  *
- * @author hebebebebe
+ * <p>
+ * The rotation is performed using an {@link AffineTransform} applied with a
+ * {@link Graphics2D} context. The output image has its width and height swapped
+ * relative to the input to accommodate the rotated content.
+ * </p>
+ *
+ * @author leuhe253
  */
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import java.awt.geom.AffineTransform;
 
-public class ImageRotation90clockwise implements ImageOperation{
-    
+public class ImageRotation90clockwise implements ImageOperation {
+
     /**
      * <p>
      * Apply a 90 degree clockwise rotation to an image.
      * </p>
      *
      * <p>
-     * The image is rotated 90 degrees clockwise about its centre point.
-     * The output image has its width and height swapped relative to the input
-     * to accommodate the rotated content.
+     * The image is rotated 90 degrees clockwise about its centre point. The
+     * output image has its width and height swapped relative to the input to
+     * accommodate the rotated content.
      * </p>
      *
      * @param input The image to rotate.
      * @return The resulting (rotated 90° clockwise) image.
      */
     public BufferedImage apply(BufferedImage input) {
-        
-        
+
         int width = input.getWidth();
         int height = input.getHeight();
 
         int newWidth = height;
         int newHeight = width;
-        
+
         BufferedImage output = new BufferedImage(newWidth, newHeight, input.getType());
-        
+
         Graphics2D g2d = output.createGraphics();
-        
+
         AffineTransform transform = new AffineTransform();
-         double angle = Math.toRadians(90);
+        double angle = Math.toRadians(90);
         transform.rotate(angle, newWidth / 2, newHeight / 2);
 
         transform.translate((newWidth - width) / 2, (newHeight - height) / 2);
-  
+
         g2d.setTransform(transform);
         g2d.drawImage(input, 0, 0, null);
         g2d.dispose();
         return output;
     }
-   
+
 }
