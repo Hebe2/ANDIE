c84e720b60186e5a4e51df960627e5f1d2e90352
manuella <manuella@manuellas-mbp-4.byod.uod.otago.ac.nz>
2026-04-18 18:12:19 +1200

    contrast mask: steps 1-4 done

--- src/main/java/cosc202/andie/Contrast_Mask.java
+++ src/main/java/cosc202/andie/Contrast_Mask.java
@@ -3,37 +3,45 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package cosc202.andie;
-import java.awt.image.*; 
-import java.io.Serializable;
 
+import java.awt.image.*;
+import java.io.Serializable;
 
 /**
  *
  * @author manuella
  */
 public class Contrast_Mask implements ImageOperation, Serializable {
-    private int radius; 
-    private double strength; 
-    
-    Contrast_Mask(int radius, double strength)
-    {
-     this.radius = radius; 
-     this.strength = strength; 
-    } 
-    
-    Contrast_Mask(){
-     this.radius = 1; 
-     this.strength = 0.9; 
+
+    private int radius;
+    private double strength;
+
+    Contrast_Mask(int radius, double strength) {
+        this.radius = radius;
+        this.strength = strength;
     }
-    
-    @Override 
-    public BufferedImage apply(BufferedImage input){
+
+    Contrast_Mask() {
+        this.radius = 1;
+        this.strength = 0.9;
+    }
+
+    @Override
+    public BufferedImage apply(BufferedImage input) {
         // deep copy 
+        ColorModel cm = input.getColorModel();
+        boolean isAlphaPremultiplied = cm.isAlphaPremultiplied();
+        WritableRaster raster = input.copyData(input.getRaster().createCompatibleWritableRaster());
+        BufferedImage mask = new BufferedImage(cm, raster, isAlphaPremultiplied, null);
         // greyscale 
+        mask = new ConvertToGrey().apply(mask);
         // inversion 
+        mask = new ImageInversion().apply(mask);
         // gaussian blur 
+        mask = new GaussianFilter().apply(mask);
         // make transparent 
+        
         // ~B~L~E~N~D~
-     return input; 
+        return input;
     }
 }
